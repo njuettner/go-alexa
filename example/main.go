c57ee7f3df2df89838a3826a31d04e188dc6ccf4
Nick Jüttner <nick.juettner@zalando.de>
2018-03-16 12:28:19 +0100

    Initial

--- example/main.go
+++ example/main.go
@@ -1,34 +1,51 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
-
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/njuettner/alexa"
 )
 
+func alexaDispatchIntentHandler(req alexa.Request) *alexa.Response {
+	switch req.RequestBody.Intent.Name {
+	case "hello":
+		return alexaGreeterIntentHandler(req)
+	default:
+		return alexaHelpHandler()
+	}
+}
+
 func main() {
+	lambda.Start(alexaDispatchIntentHandler)
+}
+
+func alexaGreeterIntentHandler(req alexa.Request) *alexa.Response {
 	simpleResponse := &alexa.SimpleResponse{
-		OutputSpeechText: "Hello World",
-		CardTitle:        "Title",
+		OutputSpeechText: "Greetings",
+		CardTitle:        "Greeter",
+		CardContent:      "Greeter Content",
+	}
+
+	//standardResponse := &alexa.StandardResponse{
+	//	OutputSpeechText: "Hello World",
+	//	CardTitle:        "Title",
+	//	CardText:         "Card Text",
+	//	CardImage: struct {
+	//		SmallImageUrl string
+	//		LargeImageUrl string
+	//	}{
+	//		SmallImageUrl: "https:/foobar.com/small_image.jpg",
+	//		LargeImageUrl: "https:/foobar.com/large_image.jpg",
+	//	},
+	//}
+
+	return alexa.NewResponse(simpleResponse)
+}
+
+func alexaHelpHandler() *alexa.Response {
+	helpResponse := &alexa.SimpleResponse{
+		OutputSpeechText: "Please ask hello to get greetings from your Alexa",
+		CardTitle:        "Help for Greeter",
 		CardContent:      "Card Content",
 	}
-
-	standardResponse := &alexa.StandardResponse{
-		OutputSpeechText: "Hello World",
-		CardTitle:        "Title",
-		CardText:         "Card Text",
-		CardImage: struct {
-			SmallImageUrl string
-			LargeImageUrl string
-		}{
-			SmallImageUrl: "https:/foobar.com/small_image.jpg",
-			LargeImageUrl: "https:/foobar.com/large_image.jpg",
-		},
-	}
-
-	resp := alexa.NewResponse(simpleResponse)
-	json.NewEncoder(os.Stdout).Encode(resp)
-	resp = alexa.NewResponse(standardResponse)
-	json.NewEncoder(os.Stdout).Encode(resp)
+	return alexa.NewResponse(helpResponse)
 }
